92b2aaf0dad48d3d7ff916a696d73aa76a490436
hyun minyoung <kingmandoo95@gmail.com>
2024-04-03 03:12:38 +0900

    init

---
+++ src/test/java/com/project/chatfiabe/ChatfiaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.chatfiabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatfiaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
