976f28dafa5520ce6e5c9df29f55b4bd8cbfb10b
hyun minyoung <kingmandoo95@gmail.com>
2024-06-03 23:47:59 +0900

    fix : RoomRepository JPL 쿼리 추가

--- src/main/java/com/project/chatfiabe/domain/room/repository/RoomRepository.java
+++ src/main/java/com/project/chatfiabe/domain/room/repository/RoomRepository.java
@@ -2,6 +2,7 @@ package com.project.chatfiabe.domain.room.repository;
 
 import com.project.chatfiabe.domain.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -12,7 +13,9 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
 
     List<Room> findAllByOrderByCreateAtDesc();
 
+    @Query("SELECT r FROM Room r LEFT JOIN r.players p GROUP BY r ORDER BY COUNT(p)")
     List<Room> findAllByOrderByPlayersSizeAsc();
 
+    @Query("SELECT r FROM Room r LEFT JOIN r.players p GROUP BY r ORDER BY COUNT(p) DESC")
     List<Room> findAllByOrderByPlayersSizeDesc();
 }
